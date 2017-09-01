a7d65e6825cbe25f50f8ce477936e3cd67b0d39a
Sam Hinshaw <samuel.hinshaw@gmail.com>
2017-09-01 09:49:32 -0700

    adding toggleable copyright modal

--- js/main.js
+++ js/main.js
@@ -1,23 +1,44 @@
+// Function 1 to listen for: hamburger menu
 document.addEventListener("DOMContentLoaded", function() {
   // Get all "navbar-burger" elements
-  var $navbarBurgers = Array.prototype.slice.call(
+  const navbarBurgers = Array.prototype.slice.call(
     document.querySelectorAll(".navbar-burger"),
     0
   );
 
   // Check if there are any nav burgers
-  if ($navbarBurgers.length > 0) {
+  if (navbarBurgers.length > 0) {
     // Add a click event on each of them
-    $navbarBurgers.forEach(function($el) {
-      $el.addEventListener("click", function() {
+    navbarBurgers.forEach(function(el) {
+      el.addEventListener("click", function() {
         // Get the target from the "data-target" attribute
-        var target = $el.dataset.target;
-        var $target = document.getElementById(target);
+        let target = el.dataset.target;
+        target = document.getElementById(target);
 
         // Toggle the class on both the "navbar-burger" and the "navbar-menu"
-        $el.classList.toggle("is-active");
-        $target.classList.toggle("is-active");
+        el.classList.toggle("is-active");
+        target.classList.toggle("is-active");
       });
     });
   }
+});
+
+// Function 2 to listen for: copyright logo/modal
+document.addEventListener("DOMContentLoaded", function() {
+  // Find the copyright logo and and the copyright modal.
+  const copyrightLogo = document.getElementById("copyrightLogo");
+  const copyrightModalClose = document.getElementById("copyrightModalClose");
+  const copyrightModal = document.getElementById("copyrightModal");
+
+  // Add an event listener to logo watch for clicks
+  copyrightLogo.addEventListener("click", function() {
+    copyrightModal.classList.toggle("is-active");
+  });
+  // Add an event listener to close button watch for clicks
+  copyrightModalClose.addEventListener("click", function() {
+    copyrightModal.classList.toggle("is-active");
+  });
+  copyrightModalBackground.addEventListener("click", function() {
+    copyrightModal.classList.toggle("is-active");
+  });
 });
