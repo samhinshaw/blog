1d56a239e103b650534c0f9e088d75c730b52480
Sam Hinshaw <samuel.hinshaw@gmail.com>
2017-09-13 19:16:03 -0700

    Add ESLint config and ligature support to code/pre tags
    - initiate npm config
    - add FiraCode webfont for code tags
    - should implement webpack/babel instead of browserify

--- js/main.js
+++ js/main.js
@@ -1,44 +1,42 @@
 // Function 1 to listen for: hamburger menu
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener('DOMContentLoaded', () => {
   // Get all "navbar-burger" elements
-  const navbarBurgers = Array.prototype.slice.call(
-    document.querySelectorAll(".navbar-burger"),
-    0
-  );
+  const navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
 
   // Check if there are any nav burgers
   if (navbarBurgers.length > 0) {
     // Add a click event on each of them
-    navbarBurgers.forEach(function(el) {
-      el.addEventListener("click", function() {
+    navbarBurgers.forEach((el) => {
+      el.addEventListener('click', () => {
         // Get the target from the "data-target" attribute
         let target = el.dataset.target;
         target = document.getElementById(target);
 
         // Toggle the class on both the "navbar-burger" and the "navbar-menu"
-        el.classList.toggle("is-active");
-        target.classList.toggle("is-active");
+        el.classList.toggle('is-active');
+        target.classList.toggle('is-active');
       });
     });
   }
 });
 
 // Function 2 to listen for: copyright logo/modal
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener('DOMContentLoaded', () => {
   // Find the copyright logo and and the copyright modal.
-  const copyrightLogo = document.getElementById("copyrightLogo");
-  const copyrightModalClose = document.getElementById("copyrightModalClose");
-  const copyrightModal = document.getElementById("copyrightModal");
+  const copyrightLogo = document.getElementById('copyrightLogo');
+  const copyrightModalClose = document.getElementById('copyrightModalClose');
+  const copyrightModal = document.getElementById('copyrightModal');
+  const copyrightModalBackground = document.getElementById('copyrightModalBackground');
 
   // Add an event listener to logo watch for clicks
-  copyrightLogo.addEventListener("click", function() {
-    copyrightModal.classList.toggle("is-active");
+  copyrightLogo.addEventListener('click', () => {
+    copyrightModal.classList.toggle('is-active');
   });
   // Add an event listener to close button watch for clicks
-  copyrightModalClose.addEventListener("click", function() {
-    copyrightModal.classList.toggle("is-active");
+  copyrightModalClose.addEventListener('click', () => {
+    copyrightModal.classList.toggle('is-active');
   });
-  copyrightModalBackground.addEventListener("click", function() {
-    copyrightModal.classList.toggle("is-active");
+  copyrightModalBackground.addEventListener('click', () => {
+    copyrightModal.classList.toggle('is-active');
   });
 });
