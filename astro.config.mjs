81c21bf6abb1ff04c548c490a4d0ca47c907d6c0
Sam Hinshaw <samuel.hinshaw@gmail.com>
2026-07-23 17:47:46 -0700

    get rid of vanilla extract

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 
 export default defineConfig({
   site: 'https://www.samhinshaw.com',
   trailingSlash: 'always',
   integrations: [sitemap()],
-  vite: { plugins: [vanillaExtractPlugin()] },
 });
