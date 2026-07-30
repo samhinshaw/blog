8ed4d29e398192e51ed8bff62c27b99d0eeb934b
Sam Hinshaw <samuel.hinshaw@gmail.com>
2026-07-30 11:31:54 -0700

    proposed changes for tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
   site: 'https://www.samhinshaw.com',
   trailingSlash: 'always',
   integrations: [sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
